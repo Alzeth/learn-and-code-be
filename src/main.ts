0a1623a1c6def985defb3aa400ffb966c0adebdc
Igor Ovcharenko <mr.alzeth@gmail.com>
2026-06-25 11:22:34 +0300

    4.0.0 Swagger integration, postman collection, readme update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +12,15 @@ async function bootstrap() {
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Learn and Code')
+    .setDescription('Learn and Code API description')
+    .setVersion('1.0')
+    .addTag('learn-and-code')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(process.env.PORT ?? 3001);
 }
